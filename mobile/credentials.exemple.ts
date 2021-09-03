b8ffe61f6fd95e4e9a2494fb4d57f2bf215792e7
marceloambarc <macelo.ambarc@gmail.com>
2021-09-03 15:28:40 -0300

    03/09 - 15:28

--- mobile/credentials.exemple.ts
+++ mobile/credentials.exemple.ts
@@ -1,9 +1,3 @@
 export const baseAPI = "host";
 export const pagSeguroAPI = "https://sandbox.api.pagseguro.com/";
 export const token = "pagSeguroUserToken";
-
-export const twoInstallment = 0.51495;
-export const threeInstallment = 0.34670;
-export const fourInstallment = 0.26255;
-export const fiveInstallment = 0.21210;
-export const sixInstallment = 0.17847;
