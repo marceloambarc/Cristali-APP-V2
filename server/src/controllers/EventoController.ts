697749080a750f218b558264ac75b4c20b520126
Marcelo Barcelos <marcelo.ambarc@gmail.com>
2022-04-27 14:03:30 -0300

    Buca por Data

--- server/src/controllers/EventoController.ts
+++ server/src/controllers/EventoController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
+import { Between, getRepository } from "typeorm";
+import { convertCompilerOptionsFromJson } from "typescript";
 import * as Yup from 'yup';
 
 import Evento from "../models/Evento";
@@ -13,20 +14,67 @@ export default {
     const events = await eventsRepository.find();
 
     if(events.length === 0){
-      return response.status(204).json({ "Vazio": "Nenhum Evento Cadastrado" });
+      return response.status(404).json({ "Vazio": "Nenhum Evento Cadastrado" });
     }else{
       return response.json(eventView.renderMany(events));
     }
   },
 
   async show(request: Request, response: Response) {
-    const { id } = request.params;
+    try{
 
-    const eventsRepository = getRepository(Evento);
+      const { id } = request.params;
 
-    const event = await eventsRepository.findOneOrFail(id);
+      const eventsRepository = getRepository(Evento);
+  
+      const event = await eventsRepository.findOneOrFail(id);
+  
+      return response.json(eventView.render(event));
 
-    return response.json(eventView.render(event));
+    }catch(err){
+      return response.status(400).json({ "Erro": err });
+    }
+  },
+
+  async showWithDt(request: Request, response: Response) {
+    try{
+
+      const { dtInicio, dtFim, dt } = request.body;
+
+      var dtInt = parseInt(dt);
+      var events = {} as Evento[];
+
+      if(dtInicio == undefined && dtFim == undefined ||
+        dtInicio == "" && dtFim == ""){
+        return response.status(400).json({ "Erro": "JSON incorreto" });
+      }
+
+      const eventsRepository = getRepository(Evento);
+
+      if(dtInt > 0){
+        events = await eventsRepository.find({
+          where: {
+            dt_evento: Between(dtInicio, dtFim)
+          },
+          take: parseInt(dt)
+        });
+      }else{
+        events = await eventsRepository.find({
+          where: {
+            dt_evento: Between(dtInicio, dtFim)
+          }
+        });
+      }
+
+      if(events.length === 0){
+        return response.status(404).json({ "Vazio": "Nenhum Evento Cadastrado" });
+      }else{
+        return response.json(eventView.renderMany(events));
+      }
+
+    }catch{
+      return response.status(510);
+    }
   },
 
   async create(request: Request, response: Response) {
--- server/src/routes/controller.routes.ts
+++ server/src/routes/controller.routes.ts
@@ -25,6 +25,7 @@ controllerRouter.post('/hashpasswords', clientAuth, SenhaController.hashPassword
 // -- LOGS
 
 controllerRouter.get('/evento', clientAuth, EventoController.index);
+controllerRouter.get('/evento/dateTime', clientAuth, EventoController.showWithDt);
 controllerRouter.get('/evento/:id', clientAuth, EventoController.show);
 controllerRouter.post('/evento', clientAuth, EventoController.create);
 
@@ -61,6 +62,6 @@ controllerRouter.post('/insertedsales', clientAuth, OrdemController.showInserted
 controllerRouter.post('/selectedpaymentsales', clientAuth, OrdemController.showSelectedPaymentSales);
 
 // -- ORDENS RESERVA
-controllerRouter.get('/bkporder', clientAuth, OrdemReservaController.index);
+controllerRouter.get('/bkporder', clientAuth, OrdemReservaController.index)
 
 export default controllerRouter;
